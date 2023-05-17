4d342ca12f7c2b1a36ff693d0ec12e4d8284caf6
Gopirg07 <113005317+Gopirg07@users.noreply.github.com>
2023-05-17 22:47:51 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -98,7 +98,7 @@ router.post("/forgetPassword", async (req, res) => {
       let token = await createForgetToken({ id: user._id });
 
       //send mail
-      const url = `http://localhost:3000/reset-password/${token}`;
+      const url = `https://gcrm-frontend.netlify.app/reset-password/${token}`;
       const name = user.name;
       const email = user.email;
       SendResetEmail(email, url, "Reset Your Password", name);
